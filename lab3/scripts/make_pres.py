a0dc7d5ca72b3734e32e1ad4e4b3f9f75dc572ac
blakedallen <blakedallen@gmail.com>
2020-07-10 00:01:30 -0700

    adding in scripts to make the pres dataset containing which party each state voted for and the percentage of votes which went to that party.

---
+++ lab3/scripts/make_pres.py
@@ -0,0 +1,30 @@
+import csv
+
+states = {}
+rows = []
+with open("pres.csv","r") as csvfile:
+    spam = csv.reader(csvfile, delimiter=",")
+    for i,row in enumerate(spam):
+        if i == 0:
+            continue
+        if row[0] != "2016":
+            continue
+
+        party = row[8]
+        if party == "democrat":
+            party = "D"
+        elif party == "republican":
+            party = "R"
+        else:
+            party = "I"
+        percent_vote = str(round(float(row[10])/float(row[11])*100,2))
+        if row[1] not in states:
+            states[row[1]] = True
+            rows.append([row[0],row[1],party, percent_vote])
+    
+with open("pres_2016.csv","w") as csvfile:
+    csvfile.write(",".join(["year","state","party","percent_vote","\n"]))
+    for row in rows:
+        csvfile.write(",".join(row)+"\n")
+
+
