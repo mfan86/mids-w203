18cd434a0e0cf4197c806b2cd1aa249e1aa24ccc
blakedallen <blakedallen@gmail.com>
2020-07-16 16:44:06 -0700

    operationalization of new variable 'election_lean', which takes into account the percent vote from the 2016 presidential election and opeationalizes the percent vote into a single continuous interval where 0 is 100% republican vote, 50% is 50-50 republican / democrat even split and 100% is all democrat vote, note: this leaves out independent votes as they are often detracting and not applicable to the final vote. The goal is to create a variable that more closely maps to the total number of party affiliates to see if there is a connection between strong party affiliation and covid infections for either Democrats or Republicans.

--- lab3/scripts/make_pres.py
+++ lab3/scripts/make_pres.py
@@ -17,13 +17,19 @@ with open("pres.csv","r") as csvfile:
             party = "R"
         else:
             party = "I"
-        percent_vote = str(round(float(row[10])/float(row[11])*100,2))
+        percent_vote = round(float(row[10])/float(row[11])*100,2)
+        #election lean is a continous variable that changes our poltical voting to a single interval 0-50 for R 50-100 for D, 
+        election_lean = percent_vote
+        if party == "D" and percent_vote > 50.0:
+            election_lean = percent_vote
+        elif party == "R" and percent_vote > 50.0:
+            election_lean = round(100.0 - percent_vote,2)
         if row[1] not in states:
             states[row[1]] = True
-            rows.append([row[0],row[1],party, percent_vote])
+            rows.append([row[0],row[1],party, str(percent_vote), str(election_lean)])
     
 with open("pres_2016.csv","w") as csvfile:
-    csvfile.write(",".join(["year","state","party","percent_vote","\n"]))
+    csvfile.write(",".join(["year","state","party","percent_vote","election_lean", "\n"]))
     for row in rows:
         csvfile.write(",".join(row)+"\n")
 
